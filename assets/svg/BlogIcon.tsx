b7577635a7f791032014e9b21c3e0c95ee1a03b0
lemessdavi <ndlemes7@outlook.com>
2025-06-09 22:21:01 -0300

    chore: lint

--- assets/svg/BlogIcon.tsx
+++ assets/svg/BlogIcon.tsx
@@ -2,22 +2,12 @@ import * as React from "react";
 import Svg, { Path, SvgProps } from "react-native-svg";
 
 const BlogIcon = (props: SvgProps) => (
-  <Svg
-    viewBox="0 0 24 24"
-    width={24}
-    height={24}
-    {...props}
-  >
+  <Svg viewBox="0 0 24 24" width={24} height={24} {...props}>
     <Path
       d="M4 2h10l6 6v14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2z"
       fill="#7B61FF"
     />
-    <Path
-      d="M14 2v6h6"
-      fill="none"
-      stroke="#fff"
-      strokeWidth={2}
-    />
+    <Path d="M14 2v6h6" fill="none" stroke="#fff" strokeWidth={2} />
     <Path
       d="M7 13h10M7 17h10M7 9h5"
       fill="none"
